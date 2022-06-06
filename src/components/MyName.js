9069823b0a05524015b8c81f5f49363d45f0e6f7
maggiemay-afk <hermsmk@gmail.com>
2022-06-05 22:28:37 -0400

    remove gatsby default, started about page

---
+++ src/components/MyName.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export const MyName = ({name, id, n}) => {
+    return (
+        <>
+            <h2>{name} {id} {n} :)</h2>
+            <p>Hello world</p>
+        </>
+    )
+}

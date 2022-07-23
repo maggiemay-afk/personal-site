93f7eb909920f745b174c215ae7ec687614d6954
maggiemay-afk <hermsmk@gmail.com>
2022-07-23 16:05:36 -0400

    test

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,6 @@
-import * as React from "react";
+import * as React from 'react';
 import Layout from '../components/Layout';
-import { Link } from "gatsby";
+import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 import {
   gridContainer,

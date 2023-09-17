2a4a0506ab1c0c2f6a3af3a2f912ec023bb3d23a
maggiemay-afk <hermsmk@gmail.com>
2023-09-17 13:44:32 -0400

    migrating to netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.maggiemay.dev`,
     description: "a personal website for Maggie Herms",
     title: "Maggie Herms"
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-netlify",
   ],
 }

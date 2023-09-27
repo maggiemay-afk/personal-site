849e8ea47dec6102b640c24224cf9f9234b827f4
maggiemay-afk <hermsmk@gmail.com>
2023-09-26 20:23:27 -0400

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,18 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-plugin-netlify",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Maggie Herms`,
+        short_name: `MH`,
+        start_url: `/`,
+        background_color: `#254E58`,
+        theme_color: `#e85a4f`,
+        display: `minimal-ui`,
+        // Generate PWA icons and a favicon
+        icon: `src/images/favicon.png`,
+      },
+    }
   ],
 }
